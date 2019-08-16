63b0ec67edbab818b4da27cb27547ff3af00cc51
jessicakwalters <jessicakwalters@gmail.com>
2019-08-15 19:54:52 -0700

    arrayReverse and tests complete

---
+++ challenges/arrayReverse/__tests__/array-reverse.test.js
@@ -0,0 +1,26 @@
+const arrayReverse = require('../array-reverse');
+
+describe('test arrayReverse function', () => {
+
+test('null array', () => {
+    let arr = [];
+    expect(arrayReverse(arr)).toEqual(-1);
+})
+
+test('non array', () => {
+    let arr = {name: 'Jessica'};
+    expect(arrayReverse(arr)).toEqual(-1);
+})
+
+test('array with even number of values', () => {
+    let arr = [1, 2, 3, 4];
+    expect(arrayReverse(arr)).toEqual([4, 3, 2, 1])
+})
+
+test('array with odd number of values', () => {
+    let arr = [1, 2, 3, 4, 5];
+    expect(arrayReverse(arr)).toEqual([5, 4, 3, 2, 1])
+})
+
+
+})
