26e016f368f06591b481d7812177b3db320365d0
jessicakwalters <jessicakwalters@gmail.com>
2019-08-19 21:03:48 -0700

    added new methods to linked list

--- Data-Structures/linkedList/__tests__/linked-list.test.js
+++ Data-Structures/linkedList/__tests__/linked-list.test.js
@@ -2,70 +2,84 @@ const linkedList = require('../linked-list');
 
 describe('Testing Liked List', () => {
 
-test('Create an empty list', () => {
-    let newList = new linkedList();
-    expect(newList.head).toEqual(null);
-})
+        let newList = new linkedList();
+        newList.insertAtHead(1);
+        newList.insertAtHead(2);
+        newList.insertAtHead(3);
+        newList.insertAtHead(4);
+        newList.insertAtHead(4);
 
-test('Test Insert into', () => {
-    let newList = new linkedList();
-    newList.insertAtHead(1);
-    expect(newList.head).toBeTruthy();
-})
+    let newList2 = new linkedList();
+    newList2.insertAtHead(1);
+    newList2.insertAtHead(2);
+    newList2.insertAtHead(3);
+    newList2.insertAtHead(4);
 
-test('Test head', () => {
-    let newList = new linkedList();
-    newList.insertAtHead(1);
-    expect(newList.head).toEqual({'next': null, 'value': 1});
-})
+    test('Create an empty list', () => {
+        let nullList = new linkedList();
+        expect(nullList.head).toEqual(null);
+    });
 
-test('Test insert multiple', () => {
-    let newList = new linkedList();
-    newList.insertAtHead(1);
-    newList.insertAtHead(2);
-    newList.insertAtHead(3);
-    newList.insertAtHead(4);
-    expect(newList.head.value).toEqual(4);
-    expect(newList.head.next.value).toEqual(3);
-})
+    test('Test Insert into', () => {
+        let oneList = new linkedList();
+        oneList.insertAtHead(1);
+        expect(oneList.head).toBeTruthy();
+    });
 
-test('Search for a value that exists in the list', () => {
-    let newList = new linkedList();
-    newList.insertAtHead(1);
-    newList.insertAtHead(2);
-    newList.insertAtHead(3);
-    newList.insertAtHead(4);
-    expect(newList.includes(3)).toEqual(true);
-})
+    test('Test head', () => {
+        let headList = new linkedList();
+        headList.insertAtHead(1);
+        expect(headList.head).toEqual({'next': null, 'value': 1});
+    });
 
-test('Search for a value that does not exists in the list', () => {
-    let newList = new linkedList();
-    newList.insertAtHead(1);
-    newList.insertAtHead(2);
-    newList.insertAtHead(3);
-    newList.insertAtHead(4);
-    expect(newList.includes(7)).toEqual(false);
-})
+    test('Test insert multiple', () => {
+        expect(newList.head.value).toEqual(4);
+        expect(newList.head.next.value).toEqual(4);
+    });
 
-test('Test toString and reveal all the items in the list', () => {
-    let newList = new linkedList();
-    newList.insertAtHead(1);
-    newList.insertAtHead(2);
-    newList.insertAtHead(3);
-    newList.insertAtHead(4);
-    newList.insertAtHead(4);
-    expect(newList.toString()).toEqual('4 4 3 2 1 ');
-})
+    test('Search for a value that exists in the list', () => {
+        expect(newList.includes(3)).toEqual(true);
+    });
 
-test('Test that remove() removes an item from the list', () => {
-    let newList = new linkedList();
-    newList.insertAtHead(1);
-    newList.insertAtHead(2);
-    newList.insertAtHead(3);
-    newList.insertAtHead(4);
-    newList.insertAtHead(4);
-    expect(newList.remove(1)).toEqual(4);
-    expect(newList.toString()).toEqual('4 3 2 1 ');
-})
+    test('Search for a value that does not exists in the list', () => {
+        expect(newList.includes(7)).toEqual(false);
+    });
 
-})
+    test('Test toString and reveal all the items in the list', () => {
+        expect(newList.toString()).toEqual('4 4 3 2 1 ');
+    });
+
+    test('Test that remove() removes an item from the list', () => {
+        expect(newList.remove(1)).toEqual(4);
+        expect(newList.toString()).toEqual('4 3 2 1 ');
+    });
+
+    test('Can successfully add a node to the end of the linked list', () => {
+        newList.append(0);
+        expect(newList.toString()).toEqual('4 3 2 1 0 ');
+    });
+
+    test('Can successfully add multiple nodes to the end of a linked list', () => {
+        newList.append(-1);
+        newList.append(-2);
+        newList.append(-3);
+        expect(newList.toString()).toEqual('4 3 2 1 0 -1 -2 -3 ');
+    });
+
+    test('Can successfully insert a node before a node located i the middle of a linked list', () => {
+        newList2.insertBefore(2, 20);
+        expect(newList2.toString()).toEqual('4 3 20 2 1 ');
+
+    });
+
+    test('Can successfully insert a node before the first node of a linked list', () => {
+        newList2.insertBefore(4, 10);
+        expect(newList2.toString()).toEqual('10 4 3 20 2 1 ');
+    });
+
+    test('Can successfully insert after a node in the middle of the linked list', () => {
+        newList2.insertAfter(2, 7);
+        expect(newList2.toString()).toEqual('10 4 3 20 2 7 1 ');
+    })
+
+});
---
+++ challenges/arrayReverse/README.md
@@ -0,0 +1,15 @@
+## Reverse an Array
+
+The idea is to reverse an array in place. 
+
+### Challenge
+
+arrayReverse: creates a function that takes in an array and returns the same array with the values in reverse order.  No array methods are used, the array is sorted in place, and empty arrays and non-array objects return -1.
+
+### Approach & Efficiency
+
+As array methods were disallowed as were array methods, I chose to use a for loop and a temp variable to iterate through the array and to swap the necessary variables.  Big O = O(N);
+
+### Solution
+
+![Image of Array Reverse](./assets/array_reverse.jpg)
