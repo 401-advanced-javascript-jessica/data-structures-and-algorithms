ea89d4fe51acb764b2dbf12612a03887e5c25dcf
jessicakwalters <jessicakwalters@gmail.com>
2019-10-28 10:26:10 -0700

    find the range sum in a binary tree

---
+++ challenges/rangeSumBST/range-sum-bst.js
@@ -0,0 +1,30 @@
+'use-strict';
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ */
+var rangeSumBST = function(root, L, R) {
+  let sum = 0;
+  if (root === null){
+    return 0;
+  }
+  if(root.val >= L && root.val <= R){
+    sum += root.val;
+    return sum + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+  } else if (root.val < L){
+    return sum + rangeSumBST(root.right, L, R);
+  } else if (root.val > R){
+    return sum + rangeSumBST(root.left, L, R);
+  }
+  return sum;
+};
